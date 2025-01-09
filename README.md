a168c6018d7f9d8b798dbb242635fa8e70c8c4ab
Lara <lara.salman01@hotmail.com>
2025-01-09 15:05:17 +0100

    version 8 - added comments and sanitization

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+# How to run the application
+
+1. Clone this repository to your local machine *(e.g Visual Studio Code)*.
+2. Build the Docker-container in *Docker Desktop*
+    - Do this by typing the following command in the terminal: **docker compose up**
+3. Once the command is executed and the build is complete, the application should be accessible by typing this in your URL: **localhost**
+4. To shut down the application, run the following command: **docker compose down**
+
+
 # Link to Figma
 
 - [Click here to be redirected to the Figma design!](https://www.figma.com/design/BHHymazghN9Ct0dlO8qcdi/U03?node-id=0-1&t=cxNihnOqaR8XzJEm-1)
--- src/functions/if-functions.php
+++ src/functions/if-functions.php
@@ -1,48 +1,71 @@
 <?php
 require_once "db.php";
 
-//Checks if the request is POST 
+//Verifies if the request is POST and if title and description are provided
 if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["title"], $_POST["description"])) {
-    
-    
-    addNewTask($_POST["title"], $_POST["description"]);
+
+    //Sanitizes user input to prevent attacks
+    $title=htmlspecialchars($_POST["title"]);
+    $description=htmlspecialchars($_POST["description"]);
+
+    //Adds a task with a title and description
+    addNewTask($title, $description);
+
+    //Redirects to homepage and stops script
     header("Location: index.php");
     exit();
 }
 
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    //Deletes the task with the given id if delete and delete_id are set
     if (isset($_POST["delete"]) && isset($_POST["delete_id"])) {
         deleteTask($conn, $_POST["delete_id"]);
-    } elseif (isset($_POST["status"]) && isset($_POST["id"])) {
+    }
+    //Updates the task with the given id
+    elseif (isset($_POST["id"])) {
         updateTask();
     }
 }
 
+//Default value is null
 $editTaskId = null;
+
+//Verifies if edit_task is set in the URL
 if (isset($_GET["edit_task"])) {
+    //Converts edit_task to an int and stores it in the variable
     $editTaskId = (int)$_GET["edit_task"];
 }
 
+//Verifies if the request is POST and if save_edit is set
 if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["save_edit"])) {
+    
+    //Retrieves task data from POST 
     $editId = $_POST["edit_id"];
     $editTitle = $_POST["edit_title"];
     $editDescription = $_POST["edit_description"];
 
+    //Updates task in the database
     editTask($conn, $editId, $editTitle, $editDescription);
 
     header("Location: index.php");
     exit();
 }
 
-
+//Verifies if the request is POST and if toggle_completed is set
 if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["toggle_complete"])) {
+    
+    //Fetch task id from POST
     $task_id = $_POST["toggle_complete"];
+
+    //Toggle if task is completed or not
     toggleTask($conn, $task_id);
+
     header("Location: " . $_SERVER["PHP_SELF"]);
     exit();
 }
 
+//Get tasks from database and store them in the variable
 $tasks = fetchTasks($conn);
 
 ?>
